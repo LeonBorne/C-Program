b3760517d29e37cc7b0bb14a67d025e44e9ce897
Vacant Neo <brawlgems100@gmail.com>
2025-08-21 14:55:55 +0545

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,26 +7,52 @@ function toggleProgramNav() {
 }
 
 // Only hide after a small delay
-totalNav.addEventListener("mouseleave", function() {
+totalNav.addEventListener("mouseleave", function () {
     hideTimeout = setTimeout(() => {
         programNav.classList.add("hidden");
     }, 200); // 200ms delay
 });
 
 // If mouse enters the header again, cancel hiding
-totalNav.addEventListener("mouseenter", function() {
+totalNav.addEventListener("mouseenter", function () {
     clearTimeout(hideTimeout);
 });
 
 // Also, prevent hiding if the mouse enters the extended nav
-programNav.addEventListener("mouseenter", function() {
+programNav.addEventListener("mouseenter", function () {
     clearTimeout(hideTimeout);
 });
 
 // Hide when leaving extended nav
-programNav.addEventListener("mouseleave", function() {
+programNav.addEventListener("mouseleave", function () {
     hideTimeout = setTimeout(() => {
         programNav.classList.add("hidden");
     }, 200);
 });
+
+
+
+function toggleBurger() {
+    const burgerHeading = document.getElementById("burger-heading");
+    const burgerTag = document.getElementById("burger-a-tag");
+    const burgerList = document.getElementById("burger-list");
+    const burgerContainer = document.getElementById("burger-container");
+    const burgerContainer_Class = document.getElementsByClassName("hidden-burger-container")[0];
+
+    burgerList.classList.toggle("hidden")
+    burgerTag.classList.toggle("hidden");
+    burgerHeading.classList.toggle("hidden");
+
+
+    if (burgerContainer === burgerContainer_Class) {
+        burgerContainer.classList.remove("hidden-burger-container");
+        burgerContainer.classList.add("open-burger-container");
+    }
+
+    else {
+        burgerContainer.classList.remove("open-burger-container");
+        burgerContainer.classList.add("hidden-burger-container");
+    }
+
+}
 
